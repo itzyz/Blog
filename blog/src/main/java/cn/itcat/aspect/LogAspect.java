a6bd210b4760cd238f132c1cc51a8f31c7c1abab
zhaoyongzhi <951815578@qq.com>
2020-05-09 20:24:07 +0800

    删除并保留

--- blog/src/main/java/cn/itcat/aspect/LogAspect.java
+++
@@ -1,9 +0,0 @@
-package cn.itcat.aspect;
-
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
-
-
-@Component
-public class LogAspect {
-}
--- spring/src/main/java/com/itheima/service/Iservice.java
+++
@@ -1,5 +0,0 @@
-package com.itheima.service;
-
-public interface Iservice {
-    void UserSave();
-}
